c89748f8f99c21579a0b48d832fca9582549e5d8
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2023-01-11 11:44:54 -0600

    creating alternative project

--- Main.java
+++ Main.java
@@ -1,53 +1,5 @@
-/*
- * Program that can numerically integrate using trapezoidal sums. 
- * Additional add-on to add manual trapezoidal sum calculator
- */
-
-import javax.swing.JOptionPane;
-
 public class Main {
     public static void main(String[] args) {
-        long lowerBound, upperBound;
-        char variable;
-        String equation;
-        long output;
-
-        // instantiates object
-        Main main = new Main();
-
-        // gets input from user
-        equation = JOptionPane.showInputDialog(null, "Enter the equation you want to integrate:");
-        // parses int for all the int
-        lowerBound = main.getLong("Enter the lower bound:");
-        upperBound = main.getLong("Enter the upper bound");
 
     }
-
-    public long getLong(String message) {
-        // checks if we have an integer, starts true so the loop will start
-        boolean noInt = true;
-
-        // what the function will return
-        long output = 0;
-
-        // variable to store input to increase readability
-        String input = "";
-
-        while (noInt) {
-            try {
-                input = JOptionPane.showInputDialog(null, message);
-                output = Integer.parseInt(input);
-            } catch (Exception e) {
-                JOptionPane.showMessageDialog(null, "Please enter a number");
-            }
-            // checks if the input is empty
-            // if so, continue the loop
-            if (input.isEmpty())
-                noInt = true;
-        }
-
-        return output;
-
-    }
-
 }
