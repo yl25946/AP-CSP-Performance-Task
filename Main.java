9ced92aa3e71eb4d26eb82f75b25c6e11e396855
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2023-01-12 11:46:06 -0600

    I fixed some bugs, and I'm tired

--- Main.java
+++ Main.java
@@ -20,27 +20,32 @@ public class Main {
         // initializes the class
         Main main = new Main();
 
-        // 0 = false
-        // 1 = true
-        int continueGame = 1;
+        // 0 = yes
+        // 1 = no
+        int continueGame = 0;
 
         // scans every guess into words
         main.scanFile("words");
 
-        JOptionPane.showMessageDialog(null,
-                "Welcome to Wordle! \nThis game will test your vocabulary! \nG is used to indicate that the letter in the guess is in the correct place. \nY is used to indicate that the letter in the guess is in the word, but not in the right place. \n* is used to indicate that the letter in the guess is not in the word.",
-                "Tutorial", -1, null);
+        while (continueGame == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Welcome to Wordle! \nThis game will test your vocabulary! \nG is used to indicate that the letter in the guess is in the correct place. \nY is used to indicate that the letter in the guess is in the word, but not in the right place. \n* is used to indicate that the letter in the guess is not in the word.",
+                    "Tutorial", -1, null);
 
-        // it will enter the if statement if you win
-        // otherwise, it will enter the else statement
-
-        while (continueGame == 1) {
+            // it will enter the if statement if you win
+            // otherwise, it will enter the else statement
             if (main.wordleGame()) {
-                JOptionPane.showConfirmDialog(null,
+                JOptionPane.showMessageDialog(null,
                         "Congratulations! \nYou guessed the word!\n The word is " + main.answer, "End", -1, null);
             } else {
-
+                JOptionPane.showMessageDialog(null,
+                        "You did not guess the word. \nThe word is " + main.answer, "End", -1, null);
             }
+
+            // yes = 0
+            // no = 1
+            continueGame = JOptionPane.showConfirmDialog(null, "Do you want to play again?", null,
+                    JOptionPane.YES_NO_OPTION, -1, null);
         }
     }
 
@@ -117,14 +122,15 @@ public class Main {
         String input;
 
         // first gets the input, and then we can go into the loop
-        input = JOptionPane.showInputDialog(null, message, title);
+        input = (String) JOptionPane.showInputDialog(null, message, title,
+                -1, null, null, null);
 
         // if word not found, continue looping until you get a valid guess
         // the for(;;) makes an infinite loop until 'I' break it
         for (;;) {
             // if the guess is empty or not in list, indicate to the user that it is not a
             // valid guess
-            if (input.isEmpty() || words.contains(input.toLowerCase())) {
+            if (input == null || input.isEmpty() || !words.contains(input.toLowerCase())) {
                 JOptionPane.showMessageDialog(null, "Please enter a 5 letter word", "Invalid Guess", -1, null);
                 input = JOptionPane.showInputDialog(null, message, title);
             } else
